a959805ef772d2f993f150d56428a87b71e03d0a
Márcio Sartini <marcio.sartini@doc88.com.br>
2021-04-21 17:48:02 -0300

    Modificado jest config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,14 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>NextJS Boilerplate</title>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <meta name="theme-color" content="#000000" />
         <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-192.png" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/img/maskable_icon.png" color="#5bbad5" />
         <meta
           name="description"
           content="A simple project to work with Typescript, React, NextJS and Styled Components"
